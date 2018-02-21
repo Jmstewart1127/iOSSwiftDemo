da243a8b130b2dcbd5d8b21993296ac02cf627c9
Jmstewart1127 <Jmstewart1127@gmail.com>
2018-02-21 16:21:34 -0600

    Initial Commit

---
+++ DemoProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoProject
+//
+//  Created by Jacob Stewart on 2/21/18.
+//  Copyright © 2018 Jacob Stewart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
